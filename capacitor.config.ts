2b6471256e3a7e41ec075741049fdff03f77b10a
th30c05 <arnaufugarolas@gmail.com>
2024-11-23 12:36:49 +0100

    feat: add android & iso support
    Signed-off-by: th30c05 <arnaufugarolas@gmail.com>

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.paeria.app',
+  appName: 'paeria',
+  webDir: 'dist'
+};
+
+export default config;
